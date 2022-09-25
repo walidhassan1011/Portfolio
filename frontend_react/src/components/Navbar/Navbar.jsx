ba2f1f2b9ad63c553fcf90c4dbf037886366b476
walidhassan1011 <walidhassan009@gmail.com>
2022-09-26 00:35:01 +0200

    working on navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { images } from "../../constants";
 import "./Navbar.scss";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["Home", "About", , "Work", "Skills", "Contact"].map((item, index) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item.toLowerCase()}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="app__navbar-menu"></div>
+    </nav>
+  );
 };
 
 export default Navbar;

09220fbad7a8bbe6cbbc57fb5b516a6187016ca1
walidhassan1011 <walidhassan009@gmail.com>
2022-09-26 23:43:13 +0200

    sanity fixed

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,29 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./About.scss";
 import { images } from "../../constants";
 import { motion } from "framer-motion";
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good web developer.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I am a good web designer.",
-    imgUrl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I am a good UI/UX designer.",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Graphic Design",
-    description: "I am a good graphic designer.",
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
+
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = "*[_type == 'abouts']";
+    client.fetch(query).then((res) => {
+      setAbouts(res);
+    });
+  }, []);
   return (
     <>
       <h2 className="head-text">
